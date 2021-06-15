f6702a1b8bf16ca830c126417ac98c16cf00cc10
Martina <martinaescorial9@gmail.com>
2021-06-15 21:25:25 +0200

    Agregar archivos de proyecto.

---
+++ UD4E4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UD4E4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 8;
+            int B = 2;
+            Console.WriteLine("El mayor es " + Math.Max(A, B));
+            
+        }
+    }
+}
